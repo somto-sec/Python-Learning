bb2b635628776ef4dffb5abf12c369285ca9cb86
Onu Somto <somtodavid930@gmail.com>
2026-01-29 07:22:17 +0100

    Update numbers.py

--- 01-Python Basics/03-Numbers/numbers.py
+++ 01-Python Basics/03-Numbers/numbers.py
@@ -1,1 +1,34 @@
+num = 3.142
+print(type(num))
+#prints the type of number in variable num 
+print(3 + 2)
+print(3 // 2)
+#two division signs drops the decimal off
+print(3 ** 2)
+#exponential
+print(3 % 2)
+print(4 % 2)
+#mod (mod answer can either be 0 or 1 i.e 0 for no remainder and 1 for the remainder)
 
+num = 1
+num+=1
+print(num)
+#adds whatever i want to the main variable
+print(abs(-3))
+#removes the negative number fromw whats in the bracket
+print(round(3.75)) 
+#rounds up the number in the bracket
+
+num_1 = 1
+num_2 = 2
+print(num_1==num_2)
+#Double equal to sign is comparison 
+print(num_1!=num_2)
+#this sign means not equal to
+
+num_1 = '100'
+num_2 = '70'
+
+num_1 = int('100')
+num_2 = int('70')
+print(num_1 + num_2)
